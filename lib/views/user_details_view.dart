8c3ff5c7f1c096cf56b839323939340524726a3a
khsujon <kamrulhasan.km3@gmail.com>
2025-02-02 23:08:41 +0600

    User details page added

---
+++ lib/views/user_details_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserDetailsView extends StatelessWidget {
+  const UserDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
