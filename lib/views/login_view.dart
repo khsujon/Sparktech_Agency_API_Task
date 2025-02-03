00aa26ae71bea1d8b90bdac283544a4b0286b690
khsujon <kamrulhasan.km3@gmail.com>
2025-02-03 12:54:34 +0600

    Custom progress indicator added

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,10 +1,53 @@
+import 'package:api_task/viewmodels/login_viewmodel.dart';
+import 'package:api_task/views/user_details_view.dart';
+import 'package:api_task/widgets/custom_loading.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({super.key});
-
+  LoginView({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final loginViewModel = Provider.of<LoginViewModel>(context);
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email')),
+            TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await loginViewModel.login(
+                      _emailController.text, _passwordController.text);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => UserDetailsView(),
+                    ),
+                  );
+                } catch (e) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(e.toString())));
+                }
+              },
+              child: Text('Login'),
+            ),
+            if (loginViewModel.isLoading)
+              CustomLoading(
+                loadingText: "Login in",
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_loading.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CustomLoading extends StatelessWidget {
+  String loadingText;
+  CustomLoading({
+    Key? key,
+    required this.loadingText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.black26,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CircularProgressIndicator(),
+          SizedBox(height: 20),
+          Text('$loadingText...',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          SizedBox(height: 10),
+          Text('Please wait a second.',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+        ],
+      ),
+    );
+  }
+}
