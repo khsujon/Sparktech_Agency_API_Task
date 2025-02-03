fe4f7ada9a1b0a190702a98ed36bee5f6ec9dc7a
khsujon <kamrulhasan.km3@gmail.com>
2025-02-03 12:18:43 +0600

    Multi provider added to main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:api_task/viewmodels/login_viewmodel.dart';
+import 'package:api_task/viewmodels/user_viewmodel.dart';
 import 'package:api_task/views/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => LoginViewModel()),
+      ChangeNotifierProvider(create: (_) => UserViewModel()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: LoginView(),
     );
   }
 }
